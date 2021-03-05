7e7ceac5ca363b28ec6a3acc858167046512a336
Bryan Emerson <bryan.l.emerson@gmail.com>
2021-03-05 16:09:23 -0500

    a commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
+import {Route, Link} from 'react-router-dom';
+import staticData from "./data/parks.json";
+// import Parks from './Parks.js';
+
+console.log(staticData)
+
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      parks: []
+    }
+  }
+
+  componentDidMount(){
+    this.setState({parks: staticData})
+  }
+
   render() {
+    const allParks = [...this.state.parks.map(park=><div key={park.name}>{park.name}</div>)]
     return (
         <div>
+          <h1>some national park stuff</h1>
+          <nav>
+            {allParks}
+          </nav>
         </div>
     );
   }
